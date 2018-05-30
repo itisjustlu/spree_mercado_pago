97e8466264027f5a6712a0743ce432a098d0f569
Lucas Juarez <lucasjavierjuarez@hotmail.com>
2018-05-30 16:30:13 -0300

    Update ticket.rb

--- app/models/spree/mercado_pago/payments/ticket.rb
+++ app/models/spree/mercado_pago/payments/ticket.rb
@@ -1,7 +1,7 @@
 class Spree::MercadoPago::Payments::Ticket < ApplicationRecord
   serialize :data, Hash
   belongs_to :user, class_name: "Spree::User", optional: true
-  has_one :payment, class_name: "Spree::Payment", as: :source, optional: true
+  has_one :payment, class_name: "Spree::Payment", as: :source
 
   def self.table_name
     "spree_mercado_pago_payments_tickets"
